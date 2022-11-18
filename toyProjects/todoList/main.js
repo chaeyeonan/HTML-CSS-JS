b8476ed3a7d5ca82d97b3e7ef4833641e3f7a671
chaeyeonan <dkscodus1204@ewhain.net>
2022-11-18 10:27:27 +0900

    feat: add&delete tasks

---
+++ toyProjects/todoList/main.js
@@ -0,0 +1,53 @@
+const addBtn = document.querySelector(".btn");
+const todoList = document.querySelector(".lists.incompleted");
+const DoneList = document.querySelector(".lists.completed");
+
+const addTodo = () => {
+  const input = document.querySelector(".input");
+  const inputTxt = input.value;
+
+  const liContents = `<li class="list">
+  <i class="ri-checkbox-circle-line"></i>
+  <p>${inputTxt}</p>
+  <span class="delete-icon"
+    ><i class="ri-delete-bin-fill"> </i
+  ></span>
+</li>`;
+
+  if (inputTxt == "") alert("할 일을 입력하세요");
+  else {
+    todoList.insertAdjacentHTML("afterbegin", liContents);
+    input.value = "";
+  }
+
+  const deleteIcon = document.querySelectorAll(".delete-icon");
+  const todoAll = document.querySelectorAll(".ri-checkbox-circle-line");
+
+  deleteIcon.forEach((item) =>
+    item.addEventListener("click", () => {
+      item.parentNode.remove();
+    })
+  );
+
+  todoAll.forEach((item) =>
+    item.addEventListener("click", () => {
+      item.parentNode.remove();
+      toggleItem(item.parentNode.textContent);
+    })
+  );
+};
+
+const toggleItem = (textContent) => {
+  DoneList.insertAdjacentHTML(
+    "afterbegin",
+    `<li class="list">
+        <i class="ri-checkbox-circle-line"></i>
+        <p>${textContent}</p>
+        <span class="delete-icon"
+          ><i class="ri-delete-bin-fill"> </i
+        ></span>
+      </li>`
+  );
+};
+
+addBtn.addEventListener("click", addTodo);
